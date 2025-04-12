cff92527bae9008ab39195336d62a66f05f91f16
yangziyiiii <2420769218@qq.com>
2025-04-12 23:06:26 +0800

    change setup smt-switch

--- contrib/setup-smt-switch.sh
+++ contrib/setup-smt-switch.sh
@@ -3,9 +3,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 DEPS=$DIR/../deps
 
-# SMT_SWITCH_VERSION=b812cc4bddddde33d2fd05f4044f4fcfb8d648d8
-# SMT_SWITCH_VERSION=1f708c2da4aa102e45848b6a349d1d3260d262a5
-
 usage () {
     cat <<EOF
 Usage: $0 [<option> ...]
@@ -72,18 +69,12 @@ if [ ! -d "$DEPS/smt-switch" ]; then
     cd $DEPS
     git clone https://github.com/yangziyiiii/smt-switch.git
     cd smt-switch
-    git checkout 9d6718f7b4e62edf3f2554d4d4c40e6ccb2f6835
     ./contrib/setup-bitwuzla.sh
-    if [ $cvc5_home = default ]; then
-        ./contrib/setup-cvc5.sh
-    fi
-    if [ $WITH_BOOLECTOR = ON ]; then
-        ./contrib/setup-btor.sh
-    fi
-    CONF_OPTS="$CONF_OPTS --cvc5-home=$(pwd)/deps/cvc5"
+    ./contrib/setup-cvc5.sh
+    # CONF_OPTS="$CONF_OPTS --cvc5-home=$(pwd)/deps/cvc5"
     
     # pass bison/flex directories from smt-switch perspective
-    ./configure.sh --bitwuzla --cvc5 $CONF_OPTS --prefix=local --static --smtlib-reader --bison-dir=../bison/bison-install --flex-dir=../flex/flex-install
+    ./configure.sh --bitwuzla --cvc5 --cvc5-home=$(pwd)/deps/cvc5 --prefix=local --static --smtlib-reader --bison-dir=../bison/bison-install --flex-dir=../flex/flex-install
     cd build
     make -j$(nproc)
     # TODO put this back
